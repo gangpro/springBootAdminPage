9c0aeb21d51c8109dd2d8ab063e5e3902723137a
gangpro <kangmh86@korea.ac.kr>
2020-03-14 02:19:49 +0900

    Lombok 설정

--- study/src/main/java/com/example/study/model/SearchParam.java
+++ study/src/main/java/com/example/study/model/SearchParam.java
@@ -1,34 +1,14 @@
 package com.example.study.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data   // 기본 생성자, getter&setter, toString 등 자동으로 생성
+@AllArgsConstructor // 모든 매개변수를 받는 생성자 생성 // SearchParam(String, String, int)
 public class SearchParam {
 
     private String account;
     private String email;
     private int page;
 
-    // 객체 파라미터를 받기 위해서는 getter&setter 생성자가 있어야 한다.
-    // getter & setter
-    public String getAccount() {
-        return account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getPage() {
-        return page;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
 }
